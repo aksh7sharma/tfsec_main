43091db93d3117aee1b4736547b7dc8457811f09
aksh7sharma <aksh.7sharma@gmail.com>
2024-01-17 16:56:46 +0100

    comments added to 3 files

--- es.tf
+++ es.tf
@@ -10,7 +10,7 @@ resource "aws_elasticsearch_domain" "monitoring-framework" {
     dedicated_master_count   = 1
   }
 
-#comment
+#comment11
   ebs_options {
     ebs_enabled = true
     volume_size = 30
